381ba63693473a5395798afe0d7fd22b9170bbc9
Patricia Zan <patricia.zan.oliveira@gmail.com>
2025-06-27 16:27:07 -0300

    First Save

---
+++ 07Exercicio/script.js
@@ -0,0 +1,17 @@
+const btn2 = document.querySelector('.bnt2')
+
+const div = document.querySelector('.div')
+
+btn2.addEventListener('click', () => {
+    console.log('aaaaaaaaaaaaaaaa');
+    div.style.display = 'block';
+    
+})
+
+const btn = document.querySelector('.btn')
+const nav = document.querySelector('.navBar')
+const lista = document.querySelector('.li')
+
+btn.addEventListener('click', () => {
+    nav.style.display = 'block'
+})
